73cb2a1791705b4987e2b568e8240b9cb75c26b7
Pranav <83628215+Pranav18262@users.noreply.github.com>
2022-01-09 19:47:12 +0530

    update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,7 +1,4 @@
-# Copyright (C) 2019-2021 The Authors
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
+# Thanks To paperplane 
 #
 """ Userbot start point """
 
